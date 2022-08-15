9761d749295a453f6229e2629aaa168c60387967
MrAmericanMike <mramericanmike@gmail.com>
2022-08-15 13:48:19 -0300

    Socketeer

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 5050;
 
 APP.use(Express.static("./public"));
 
+APP.get("/ping", (req, res) => {
+	res.send("<h1>Pong</h1>");
+});
+
 IO.on("connection", async (socket) => {
 	console.log("Client connected");
 
@@ -15,12 +19,10 @@ IO.on("connection", async (socket) => {
 		console.log("Ping received");
 		cb("Pong");
 	});
-	socket.on("disconnect", () => {
-		console.log("Client disconnected");
+	socket.on("disconnect", (data) => {
+		console.log("Client disconnected", data);
 	});
 });
-
-
 
 SERVER.listen(PORT, () => {
 	console.log(`Listening on PORT ${PORT}`);
